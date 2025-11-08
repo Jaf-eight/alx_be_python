01a6743fe01d719f09461de97cc9746e77913127
Jaf-eight <yafet.eth@gmail.com>
2025-11-08 18:42:11 +0300

    initial python code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#Basic Opreation
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#Outputs
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2}is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
